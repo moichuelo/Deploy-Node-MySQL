ca740d92096a673ef3af070f12df9e9fd3cf3671
Moisés Macías Díaz <moises@mardesin.com>
2024-11-13 14:36:41 +0100

    cookie-session, scripts y entorno de produccion

--- public/js/funcionescliente.js
+++ public/js/funcionescliente.js
@@ -1,7 +1,9 @@
-function multiplicar(){
+//Función que utilizaremos del lado del cliente para multiplicar dos números
+function multiplicar() {
     var nume1 = document.getElementById("nume1").value;
     var nume2 = document.getElementById("nume2").value;
     var total = parseFloat(nume1) * parseFloat(nume2);
 
-    document.getElementById("total").innerHTML = "Total de la multiplicación: " + total;
+    document.getElementById("total").innerHTML =
+        "Total de la multiplicación: " + total;
 }
